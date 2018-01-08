98be056b56282b08b07ddf430e3b64a88483722d
Mael Pedretti <mael.pedretti@hotmail.fr>
2018-01-08 08:27:59 +0100

    typed variables
    Added if
    Adde for and while implemented in parser

--- Compiler/parser5.py
+++ Compiler/parser5.py
@@ -1,4 +1,3 @@
-"""Code parser."""
 import os
 import sys
 
@@ -24,94 +23,131 @@ vars = {}
 
 
 def p_programme(p):
-    """
-    programme : statement ';'
-    |  statement ';' programme ';'
-    """
+    ''' programme : statement
+    |  statement programme '''
     try:
-        p[0] = AST.ProgramNode([p[1]] + p[3].children)
+        p[0] = AST.ProgramNode([p[1]] + p[2].children)
     except Exception as e:
         p[0] = AST.ProgramNode(p[1])
 
 
 def p_statement(p):
-    """
-    statement : assignation
+    ''' statement : assignation
         | structure
-        | PRINT expression
-    """
+        | condition
+        | PRINT expression ';' '''
     try:
         p[0] = AST.PrintNode(p[2])
     except Exception as e:
         p[0] = p[1]
 
 
+def p_condition_if(p):
+    ''' condition_if : expression '<' expression
+        | expression '>' expression
+        | expression EQUALS expression
+        | expression LESSTHAN expression
+        | expression GREATTHAN expression
+        | expression NOTEQUAL expression
+        '''
+    p[0] = AST.IfConditionNode(p[2], [p[1], p[3]])
+
+
+def p_condition(p):
+    ''' condition : IF '(' condition_if ')' '{' programme '}' '''
+    p[0] = AST.IfNode([p[3], p[6]])
+
+
+def p_structure_while_cond(p):
+    ''' structure : WHILE '(' condition_if ')' '{' programme '}' '''
+    p[0] = AST.WhileNode([p[3], p[6]])
+
+
 def p_structure(p):
-    """
-    structure : WHILE expression '{' programme '}'
-    """
+    ''' structure : WHILE expression '{' programme '}' '''
     p[0] = AST.WhileNode([p[2], p[4]])
 
 
+def p_for(p):
+    ''' structure : FOR '(' assignation condition_if ';' \
+    assignation ')' '{' programme '}' '''
+    p[0] = AST.ForNode([p[3], p[4], p[6], p[9]])
+
+
 def p_expression_op(p):
-    """
-    expression : expression ADD_OP expression
-        | expression MUL_OP expression
-    """
+    '''expression : expression ADD_OP expression
+            | expression MUL_OP expression'''
     p[0] = AST.OpNode(p[2], [p[1], p[3]])
 
 
-def p_expression_comp(p):
-    """
-    expression : expression '<' expression
-        | expression '>' expression
-        | expression DOUBLE_EQUAL expression
-    """
-    p[0] = AST.CompNode(p[2], [p[1], p[3]])
+def p_expression_bool(p):
+    ''' expression : TRUE
+            | FALSE '''
+    p[0] = AST.TokenNode(p[1])
 
 
 def p_expression_num(p):
-    """expression : NUMBER"""
+    ''' expression : NUMBERAL '''
     p[0] = AST.TokenNode(p[1])
 
 
 def p_expression_var(p):
-    """expression : IDENTIFIER"""
+    ''' expression : IDENTIFIER '''
     p[0] = AST.TokenNode(p[1])
 
 
 def p_expression_paren(p):
-    """expression : '(' expression ')'"""
+    '''expression : '(' expression ')' '''
     p[0] = p[2]
 
 
 def p_minus(p):
-    """expression : ADD_OP expression %prec UMINUS"""
+    ''' expression : ADD_OP expression %prec UMINUS'''
     p[0] = AST.OpNode(p[1], [p[2]])
 
 
+def p_assign_number(p):
+    ''' assignation : NUMBER IDENTIFIER '=' expression ';' '''
+    vars[p[2]] = ('NUMBER', p[4])
+    p[0] = AST.AssignInitNode(p[1], [AST.TokenNode(p[2]), p[4]])
+
+
+def p_assign_string(p):
+    ''' assignation : STRING IDENTIFIER '=' TEXT ';' '''
+    vars[p[2]] = ('STRING', p[4])
+    p[0] = AST.AssignInitNode(p[1], [AST.TokenNode(p[2]), AST.TokenNode(p[4])])
+
+
+def p_assign_bool(p):
+    ''' assignation : BOOL IDENTIFIER '=' TRUE ';'
+        | BOOL IDENTIFIER '=' FALSE ';'
+    '''
+    vars[p[2]] = ('BOOL', p[4])
+    p[0] = AST.AssignInitNode(p[1], [AST.TokenNode(p[2]), AST.TokenNode(p[4])])
+
+
 def p_assign(p):
-    """assignation : IDENTIFIER '=' expression"""
-    vars[p[1]] = p[3]
+    ''' assignation : IDENTIFIER '=' expression ';' '''
+    vars[p[1]] = (vars[p[1]][0], p[3])
     p[0] = AST.AssignNode([AST.TokenNode(p[1]), p[3]])
 
 
 def p_error(p):
-    """Catch errors."""
     print("Syntax error in line %d" % p.lineno)
-    parser.errok()
 
 
 def parse(program):
-    """Parse programm."""
     return yacc.parse(program)
 
 
 parser = yacc.yacc(outputdir='generated')
 
 if __name__ == "__main__":
+    import time
+
     prog = open(sys.argv[1]).read()
     result = yacc.parse(prog, debug=1)
+    time.sleep(1)
     print(result)
 
     graph = result.makegraphicaltree()
