cc192bdf1617fb8c4393a7a0260ee3b097c72a5d
Mael Pedretti <mael.pedretti@hotmail.fr>
2018-01-10 10:26:16 +0100

    Minor updates
    No need for the last semicolon in for clause anymore
    Error handling when function or variable were not declared before

--- Compiler/parser5.py
+++ Compiler/parser5.py
@@ -33,7 +33,7 @@ def p_programme(p):
 
 
 def p_statement(p):
-    """ statement : assignation
+    """ statement : assign
         | structure
         | condition
         | fct
@@ -71,9 +71,9 @@ def p_structure(p):
 
 
 def p_for(p):
-    """ structure : FOR '(' assignation condition_if ';' \
-    assignation ')' '{' programme '}' """
-    p[0] = AST.ForNode([p[3], p[4], p[6], p[9]])
+    """ structure : FOR '(' fct_assign ';' condition_if ';' \
+    fct_assign ')' '{' programme '}' """
+    p[0] = AST.ForNode([p[3], p[5], p[7], p[10]])
 
 
 def p_fct_def(p):
@@ -96,14 +96,24 @@ def p_fct(p):
     """
     fct : IDENTIFIER '(' ')' ';'
     """
-    p[0] = AST.FunctionNode(p[1], [AST.FunctionParametersNode([])])
+    if p[1] in fcts:
+        p[0] = AST.FunctionNode(p[1], [AST.FunctionParametersNode([])])
+    else:
+        exit("Syntax error near line %s\n>> Function \"%s\" was not declared" %
+             (p.lineno(1), p[1]))
+        raise SyntaxError(p)
 
 
 def p_fct_params(p):
     """
     fct : IDENTIFIER '(' fct_expression ')' ';'
     """
-    p[0] = AST.FunctionNode(p[1], [AST.FunctionParametersNode(p[3])])
+    if p[1] in fcts:
+        p[0] = AST.FunctionNode(p[1], [AST.FunctionParametersNode(p[3])])
+    else:
+        exit("Syntax error near line %s\n>> Function \"%s\" was not declared" %
+             (p.lineno(1), p[1]))
+        raise SyntaxError(p)
 
 
 def p_fct_expression(p):
@@ -173,30 +183,45 @@ def p_expression_fct(p):
     p[0] = AST.AssignInitNode(p[1], [AST.TokenNode(p[2]), AST.TokenNode(None)])
 
 
+def p_assign_semicolon(p):
+    """assign : assignation ';'"""
+    p[0] = p[1]
+
+
+def p_assign_fct(p):
+    """fct_assign : assignation """
+    p[0] = p[1]
+
+
 def p_assign_number(p):
-    """ assignation : NUMBER IDENTIFIER '=' expression ';' """
+    """ assignation : NUMBER IDENTIFIER '=' expression """
     vars[p[2]] = ('NUMBER', p[4])
     p[0] = AST.AssignInitNode(p[1], [AST.TokenNode(p[2]), p[4]])
 
 
 def p_assign_string(p):
-    """ assignation : STRING IDENTIFIER '=' TEXT ';' """
+    """ assignation : STRING IDENTIFIER '=' TEXT """
     vars[p[2]] = ('STRING', p[4])
     p[0] = AST.AssignInitNode(p[1], [AST.TokenNode(p[2]), AST.TokenNode(p[4])])
 
 
 def p_assign_bool(p):
-    """ assignation : BOOL IDENTIFIER '=' TRUE ';'
-        | BOOL IDENTIFIER '=' FALSE ';'
+    """ assignation : BOOL IDENTIFIER '=' TRUE
+        | BOOL IDENTIFIER '=' FALSE
     """
     vars[p[2]] = ('BOOL', p[4])
     p[0] = AST.AssignInitNode(p[1], [AST.TokenNode(p[2]), AST.TokenNode(p[4])])
 
 
 def p_assign(p):
-    """ assignation : IDENTIFIER '=' expression ';' """
-    vars[p[1]] = (vars[p[1]][0], p[3])
-    p[0] = AST.AssignNode([AST.TokenNode(p[1]), p[3]])
+    """ assignation : IDENTIFIER '=' expression """
+    try:
+        vars[p[1]] = (vars[p[1]][0], p[3])
+        p[0] = AST.AssignNode([AST.TokenNode(p[1]), p[3]])
+    except KeyError as e:
+        exit("Syntax error near line %s\n>> Token \"%s\" was not declared" %
+             (p.lineno(1), p[1]))
+        raise SyntaxError(p)
 
 
 def p_expression_text(p):
@@ -205,7 +230,14 @@ def p_expression_text(p):
 
 
 def p_error(p):
-    print("Syntax error in line %d" % p.lineno)
+    if p:
+        tok = parser.token()
+        print("Syntax error at token \"%s\" near line %s" % (p.type, p.lineno))
+        # Just discard the token and tell the parser it's okay.
+        parser.errok()
+        return tok
+    else:
+        print("Syntax error at EOF")
 
 
 def parse(program):
